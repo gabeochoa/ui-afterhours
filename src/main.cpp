6986c530d2828bd547882ee5021c019fdf8aa4f9
Gabe Ochoa <gabeochoa@meta.com>
2024-12-16 22:43:05 -0500

    add ui context with focus and hover states

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 //
 #include <iostream>
+#include <set>
 
 #include "rl.h"
 //
@@ -34,6 +35,10 @@ template <class... Ts> struct overloaded : Ts... {
 
 template <typename T> int sgn(T val) { return (T(0) < val) - (val < T(0)); }
 } // namespace util
+
+// TODO i kinda dont like that you have to do this even if you dont want to
+// customize it
+struct EQ : public EntityQuery<EQ> {};
 
 struct RenderFPS : System<window_manager::ProvidesCurrentResolution> {
   virtual ~RenderFPS() {}
@@ -90,8 +95,95 @@ struct RenderAvailableResolutions
 };
 
 namespace UI {
+inline bool is_mouse_inside(const input::MousePosition &mouse_pos,
+                            const raylib::Rectangle &rect) {
+  return mouse_pos.x >= rect.x && mouse_pos.x <= rect.x + rect.width &&
+         mouse_pos.y >= rect.y && mouse_pos.y <= rect.y + rect.height;
+}
 
 struct UIComponent : BaseComponent {};
+struct UIContext : BaseComponent {
+  EntityID ROOT = -1;
+  EntityID FAKE = -2;
+
+  std::set<EntityID> focused_ids;
+
+  EntityID hot_id = ROOT;
+  EntityID focus_id = ROOT;
+  EntityID active_id = ROOT;
+  EntityID last_processed = ROOT;
+
+  input::MousePosition mouse_pos;
+  bool mouseLeftDown;
+
+  [[nodiscard]] bool is_hot(EntityID id) { return hot_id == id; };
+  [[nodiscard]] bool is_active(EntityID id) { return active_id == id; };
+  void set_hot(EntityID id) { hot_id = id; }
+  void set_active(EntityID id) { active_id = id; }
+
+  bool has_focus(EntityID id) { return focus_id == id; }
+  void set_focus(EntityID id) { focus_id = id; }
+
+  void active_if_mouse_inside(EntityID id, raylib::Rectangle rect) {
+    if (is_mouse_inside(mouse_pos, rect)) {
+      set_hot(id);
+      if (is_active(ROOT) && mouseLeftDown) {
+        set_active(id);
+      }
+    }
+  }
+
+  void reset() {
+    focus_id = ROOT;
+    last_processed = ROOT;
+    hot_id = ROOT;
+    active_id = ROOT;
+    focused_ids.clear();
+  }
+
+  void try_to_grab(EntityID id) {
+    focused_ids.insert(id);
+    if (has_focus(ROOT)) {
+      set_focus(id);
+    }
+  }
+
+  [[nodiscard]] bool is_mouse_click(EntityID id) {
+    bool let_go = !mouseLeftDown;
+    bool was_click = let_go && is_active(id) && is_hot(id);
+    // if(was_click){play_sound();}
+    return was_click;
+  }
+};
+
+struct BeginUIContextManager : System<UIContext> {
+  virtual void for_each_with(Entity &, UIContext &context, float) override {
+    context.mouse_pos = input::get_mouse_position();
+    context.mouseLeftDown = input::is_mouse_button_down(0);
+
+    context.hot_id = context.ROOT;
+  }
+};
+
+struct EndUIContextManager : System<UIContext> {
+
+  virtual void for_each_with(Entity &, UIContext &context, float) override {
+
+    if (context.focus_id == context.ROOT)
+      return;
+
+    if (context.mouseLeftDown) {
+      if (context.is_active(context.ROOT)) {
+        context.set_active(context.FAKE);
+      }
+    } else {
+      context.set_active(context.ROOT);
+    }
+    if (!context.focused_ids.contains(context.focus_id))
+      context.focus_id = context.ROOT;
+    context.focused_ids.clear();
+  }
+};
 
 struct Transform : BaseComponent {
   vec2 position;
@@ -99,6 +191,11 @@ struct Transform : BaseComponent {
   Transform(vec2 pos, vec2 sz) : position(pos), size(sz) {}
   raylib::Rectangle rect() const {
     return raylib::Rectangle{position.x, position.y, size.x, size.y};
+  }
+
+  raylib::Rectangle focus_rect(int rw = 2) const {
+    return raylib::Rectangle{position.x - rw, position.y - rw,
+                             size.x + (2 * rw), size.y + (2 * rw)};
   }
 };
 
@@ -113,39 +210,54 @@ struct HasClickListener : BaseComponent {
   HasClickListener(const std::function<void(void)> &callback) : cb(callback) {}
 };
 
-inline bool is_mouse_inside(const input::MousePosition &mouse_pos,
-                            const raylib::Rectangle &rect) {
-  return mouse_pos.x >= rect.x && mouse_pos.x <= rect.x + rect.width &&
-         mouse_pos.y >= rect.y && mouse_pos.y <= rect.y + rect.height;
-}
+template <typename... Components>
+struct SystemWithUIContext : System<Components...> {
+  Entity *context_entity;
+  virtual void once(float) override {
+    OptEntity opt_context = EQ().whereHasComponent<UIContext>().gen_first();
+    context_entity = opt_context.value();
+  }
+};
 
-struct HandleClicks : System<Transform, HasClickListener> {
+struct HandleClicks : SystemWithUIContext<Transform, HasClickListener> {
   virtual ~HandleClicks() {}
-  virtual void for_each_with(Entity &, Transform &transform,
+
+  virtual void for_each_with(Entity &entity, Transform &transform,
                              HasClickListener &hasClickListener,
                              float) override {
+    if (!context_entity)
+      return;
+    UIContext &context = context_entity->get<UIContext>();
 
-    bool is_inside =
-        is_mouse_inside(input::get_mouse_position(), transform.rect());
-    bool down = input::is_mouse_button_down(0);
+    context.active_if_mouse_inside(entity.id, transform.rect());
+    context.try_to_grab(entity.id);
+    // draw focus ring
+    // handle tabbing
 
-    if (is_inside && down && !hasClickListener.down) {
+    if (context.is_mouse_click(entity.id)) {
+      context.set_focus(entity.id);
       hasClickListener.cb();
-      hasClickListener.down = true;
-    }
-
-    if (!down) {
-      hasClickListener.down = false;
     }
   }
 };
 
-struct RenderUI : System<Transform> {
+struct RenderUI : SystemWithUIContext<UIComponent, Transform, HasColor> {
   virtual ~RenderUI() {}
-  virtual void for_each_with(const Entity &entity, const Transform &transform,
-                             float) const override {
-    raylib::Color col =
-        entity.has<HasColor>() ? entity.get<HasColor>().color : raylib::BLACK;
+  virtual void for_each_with(const Entity &entity, const UIComponent &,
+                             const Transform &transform,
+                             const HasColor &hasColor, float) const override {
+    if (!context_entity)
+      return;
+    UIContext &context = context_entity->get<UIContext>();
+
+    raylib::Color col = hasColor.color;
+    if (context.is_hot(entity.id)) {
+      col = raylib::RED;
+    }
+
+    if (context.has_focus(entity.id)) {
+      raylib::DrawRectangleRec(transform.focus_rect(), raylib::PINK);
+    }
     raylib::DrawRectangleV(transform.position, transform.size, col);
   }
 };
@@ -165,6 +277,7 @@ int main(void) {
     window_manager::add_singleton_components(
         entity, window_manager::Resolution{screenWidth, screenHeight}, 200,
         get_resolutions());
+    entity.addComponent<UI::UIContext>();
   }
 
   // make_button
@@ -182,7 +295,9 @@ int main(void) {
   // debug systems
   { window_manager::enforce_singletons(systems); }
 
+  systems.register_update_system(std::make_unique<UI::BeginUIContextManager>());
   systems.register_update_system(std::make_unique<UI::HandleClicks>());
+  systems.register_update_system(std::make_unique<UI::EndUIContextManager>());
 
   // renders
   {
